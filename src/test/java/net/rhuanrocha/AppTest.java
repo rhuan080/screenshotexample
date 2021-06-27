11aa9df1da5fcfbb5d39c4c43fcd46ea6d1d5274
Rhuan Rocha <rhuan080@gmail.com>
2021-06-27 00:21:26 -0300

    Adding test to 50% of screen

--- src/test/java/net/rhuanrocha/AppTest.java
+++ src/test/java/net/rhuanrocha/AppTest.java
@@ -36,4 +36,22 @@ public class AppTest
         ImageIO.write(capture, "bmp", imageFile );
         assertTrue(imageFile .exists());
     }
+
+    @Test
+    public void shouldExtractScreenShot50Percent() throws Exception {
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth()/2;
+        int height = gd.getDisplayMode().getHeight()/2;
+        int x = gd.getDisplayMode().getWidth()/2;
+        int y = 0;
+
+        Rectangle screenRect = new Rectangle(x,y,width,height);
+        BufferedImage capture = new Robot().createScreenCapture(screenRect);
+
+        File imageFile = new File("single-screen-50percent.bmp");
+        ImageIO.write(capture, "bmp", imageFile );
+        assertTrue(imageFile .exists());
+    }
+
+
 }
